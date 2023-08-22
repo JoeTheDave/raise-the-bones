950608fe3e8d41939362022acca2a8f8fd726dea
Joe Davis <jdavis@grayshift.com>
2023-08-22 13:38:45 -0500

    launch basic express server

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,76 @@
 #!/usr/bin/env node
 import fs from 'fs';
-import { spawn } from 'child_process';
+import { exec, spawn } from 'child_process';
 import gradient from 'gradient-string';
 import inquirer from 'inquirer';
+import { createSpinner } from 'nanospinner';
+import * as fileContent from './fileContent.js';
 console.log(gradient('#666', '#FFF', '#666')('RAISE THE BONES!'));
-let projectName = '';
 const askProjectName = async () => {
     const answers = await inquirer.prompt({
         name: 'projectName',
         type: 'input',
         message: 'Project Name?',
     });
-    projectName = answers.projectName;
+    return answers.projectName;
 };
-askProjectName().then(() => {
-    if (projectName && !fs.existsSync(projectName)) {
-        fs.mkdirSync(projectName);
-        const proc = spawn('npm init -y', { cwd: `${process.cwd()}/${projectName}` });
-        proc.stdout.on('data', data => console.log(data));
-        proc.stderr.on('data', data => console.log(data));
-        proc.on('close', () => console.log('Ending command'));
-    }
+const executeCommand = (command, args, executionDirectory) => new Promise(resolve => {
+    const child = spawn(command, args, { cwd: executionDirectory });
+    child.on('close', () => {
+        resolve();
+    });
 });
+const installNodeDependencies = async (directory) => {
+    const spinner = createSpinner('Installing dependencies...').start();
+    await executeCommand('npm', ['init', '-y'], directory);
+    await executeCommand('npm', ['i', 'express', 'ts-node', 'typescript'], directory);
+    await executeCommand('npm', [
+        'i',
+        '-D',
+        '@types/node',
+        '@types/express',
+        'eslint',
+        '@typescript-eslint/eslint-plugin',
+        '@typescript-eslint/parser',
+        'eslint-config-airbnb-typescript',
+    ], directory);
+    await executeCommand('npx', ['--yes', 'install-peerdeps', '--dev', 'eslint-config-airbnb'], directory);
+    spinner.success();
+};
+const addConfigurationFiles = (directory, projectName) => {
+    const spinner = createSpinner('Adding configuration files ...').start();
+    exec('node -v', (err, nodeVersion) => {
+        if (!err) {
+            fs.writeFileSync(`${directory}/.nvmrc`, nodeVersion.replace('v', ''));
+        }
+    });
+    fs.writeFileSync(`${directory}/.gitignore`, fileContent.gitIgnore());
+    fs.writeFileSync(`${directory}/.prettierrc`, fileContent.prettierrc());
+    fs.writeFileSync(`${directory}/.eslintrc.json`, fileContent.eslint());
+    fs.writeFileSync(`${directory}/tsconfig.json`, fileContent.tsConfig());
+    fs.mkdirSync(`${directory}/src`);
+    fs.writeFileSync(`${directory}/src/server.ts`, fileContent.serverTs());
+    fs.writeFileSync(`${directory}/src/index.html`, fileContent.indexHtml(projectName));
+    const packageJson = JSON.parse(fs.readFileSync(`${directory}/package.json`, { encoding: 'utf8', flag: 'r' }));
+    packageJson.scripts = {
+        start: 'ts-node --esm src/server.ts',
+    };
+    packageJson.type = 'module';
+    fs.writeFileSync(`${directory}/package.json`, JSON.stringify(packageJson, null, 2));
+    spinner.success();
+};
+const initializeGit = async (directory) => {
+    const spinner = createSpinner('Initializing git...').start();
+    await executeCommand('git', ['init'], directory);
+    await executeCommand('git', ['add', '-A'], directory);
+    await executeCommand('git', ['commit', '-m', 'initializing project with raise-the-bones'], directory);
+    spinner.success();
+};
+const projectName = await askProjectName();
+const projectDirectory = `${process.cwd()}/${projectName}`;
+if (projectName && !fs.existsSync(projectName)) {
+    fs.mkdirSync(projectDirectory);
+    await installNodeDependencies(projectDirectory);
+    addConfigurationFiles(projectDirectory, projectName);
+    await initializeGit(projectDirectory);
+}
---
+++ src/fileContent.ts
@@ -0,0 +1,107 @@
+export const indexHtml = (projectName: string) => `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>${projectName}</title>
+    <style>
+      html,
+      body {
+        padding: 0px;
+        margin: 0px;
+      }
+      .container {
+        width: 800px;
+        height: 400px;
+        background-color: #eee;
+        border-radius: 20px;
+        margin: 200px auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <h1>${projectName}</h1>
+      Built with <a href="https://www.npmjs.com/package/raise-the-bones">raise-the-bones</a>
+    </div>
+  </body>
+</html>
+`
+
+export const gitIgnore = () => `node_modules
+`
+
+export const serverTs = () => `import express, { Request, Response } from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.join(dirname, './index.html'))
+})
+
+app.listen(port)
+console.log(\`Server started at http://localhost: \${port}\`)
+`
+
+export const prettierrc = () => `{
+  "singleQuote": true,
+  "trailingComma": "all",
+  "arrowParens": "avoid",
+  "semi": false,
+  "printWidth": 120
+}
+`
+
+export const eslint = () => `{
+  "rules": {
+    "react-hooks/exhaustive-deps": "off",
+    "no-loop-func": "off",
+    "no-console": "off",
+    "@typescript-eslint/semi": "off",
+    "arrow-parens": "off",
+    "no-multiple-empty-lines": "off",
+    "no-multi-spaces": "off",
+    "no-trailing-spaces": "off",
+    "implicit-arrow-linebreak": "off",
+    "padded-blocks": "off"
+  },
+  "extends": ["airbnb", "airbnb-typescript"],
+  "parserOptions": {
+    "project": "./tsconfig.json"
+  },
+  "ignorePatterns": ["dist/*.js"]
+}
+`
+
+export const tsConfig = () => `{
+  "include": ["**/*.ts"],
+  "compilerOptions": {
+    "isolatedModules": true,
+    "esModuleInterop": true,
+    "moduleResolution": "node",
+    "resolveJsonModule": true,
+    "target": "esnext",
+    "module": "esnext",
+    "strict": true,
+    "allowJs": true,
+    "forceConsistentCasingInFileNames": true,
+    "baseUrl": ".",
+    "skipLibCheck": true,
+    "allowSyntheticDefaultImports": true,
+    "paths": {
+      "~/*": ["./src/*"]
+    },
+    "outDir": "dist"
+  }
+}
+`
